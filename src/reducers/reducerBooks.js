05dd9fa4964405dc0792a8e180e5c22cbf4b76cc
Yago Pérez Vázquez <yago.perezv@tecnocom.es>
2017-07-19 15:28:33 +0200

    Creating first container

---
+++ src/reducers/reducerBooks.js
@@ -0,0 +1,8 @@
+export default () => {
+    return [
+        {title: 'Javascript the good parts'},
+        {title: 'Harry Potter'},
+        {title: 'Eloquent Ruby'},
+        {title: 'The Dark Tower'}
+    ]
+}
