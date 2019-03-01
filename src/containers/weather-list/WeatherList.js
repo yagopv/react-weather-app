82b4177cb1fb392ed29f09f5ffa864b3e328ea30
Yago Pérez Vázquez <yagoperez@outlook.com>
2019-03-01 17:36:39 +0100

    [UPDATE] WeatherList

--- src/containers/weather-list/WeatherList.js
+++ src/containers/weather-list/WeatherList.js
@@ -1,55 +1,54 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 
 import styles from './WeatherList.module.scss';
 import Chart from '../../components/chart/Chart';
 import GoogleMap from '../../components/google-map/GoogleMap';
 
-class WeatherList extends Component {
-  renderWeather(cityData) {
-    const { id } = cityData.city;
-    const { lat, lon } = cityData.city.coord;
-    const temps = cityData.list.map(weather => weather.main.temp);
-    const humidities = cityData.list.map(weather => weather.main.humidity);
-    const pressures = cityData.list.map(weather => weather.main.pressure);
+function WeatherRow({ data }) {
+  const { lat, lon } = data.city.coord;
+  const temps = data.list.map(weather => weather.main.temp);
+  const humidities = data.list.map(weather => weather.main.humidity);
+  const pressures = data.list.map(weather => weather.main.pressure);
 
-    return (
-      <tr key={id}>
-        <td className={styles.bodyCell}>
-          <GoogleMap lat={lat} lon={lon} />
-        </td>
-        <td className={styles.bodyCell}>
-          <Chart data={temps} color="red" units="&#8451;" />
-        </td>
-        <td className={styles.bodyCell}>
-          <Chart data={humidities} color="orange" units="%" />
-        </td>
-        <td className={styles.bodyCell}>
-          <Chart data={pressures} color="white" units="hPA" />
-        </td>
-      </tr>
-    );
+  return (
+    <tr>
+      <td className={styles.bodyCell}>
+        <GoogleMap lat={lat} lon={lon} />
+      </td>
+      <td className={styles.bodyCell}>
+        <Chart data={temps} color="red" units="&#8451;" />
+      </td>
+      <td className={styles.bodyCell}>
+        <Chart data={humidities} color="orange" units="%" />
+      </td>
+      <td className={styles.bodyCell}>
+        <Chart data={pressures} color="white" units="hPA" />
+      </td>
+    </tr>
+  );
+}
+
+function Loading({ isFetching }) {
+  return isFetching ? <p className={styles.loading}>Loading ...</p> : '';
+}
+
+function Error({ error }) {
+  if (!error) {
+    return null;
   }
 
-  renderLoading(isFetching) {
-    return isFetching ? <p className={styles.loading}>Loading ...</p> : '';
-  }
+  return <p className={styles.error}>{error.toUpperCase()}</p>;
+}
 
-  renderError(error) {
-    if (!error) {
-      return null;
-    }
+function WeatherList({ weather }) {
+  const { weatherData, error, isFetching } = weather;
 
-    return <p className={styles.error}>{error.toUpperCase()}</p>;
-  }
-
-  render() {
-    const { weatherData, error, isFetching } = this.props.weather;
-
-    return (
-      <div className={styles.weatherListContainer}>
-        {this.renderLoading(isFetching)}
-        {this.renderError(error)}
+  return (
+    <div className={styles.weatherListContainer}>
+      <Loading isFetching={isFetching} />
+      <Error error={error} />
+      {weatherData.length > 0 && (
         <table className={styles.weatherList}>
           <thead>
             <tr className={styles.headerRow}>
@@ -59,11 +58,15 @@ class WeatherList extends Component {
               <th className={styles.headerCell}>Pressures (hPa)</th>
             </tr>
           </thead>
-          <tbody>{weatherData.map(this.renderWeather)}</tbody>
+          <tbody>
+            {weatherData.map(weather => (
+              <WeatherRow data={weather} key={weather.city.id} />
+            ))}
+          </tbody>
         </table>
-      </div>
-    );
-  }
+      )}
+    </div>
+  );
 }
 
 function mapStateToProps({ weather }) {
